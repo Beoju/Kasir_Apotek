bcc5e3d36e41f5b14afd75e86fa249671f898e9b
desikaaa <desikarmila211@gmail.com>
2023-12-11 20:30:27 +0700

    laporan transaksi

--- kasir2.java
+++ kasir2.java
@@ -8,45 +8,63 @@ public class kasir2 {
         String[] daftarObat = {"Paracetamol", "Vitamin C", "Antibiotik", "Obat Flu"};
         int[] hargaObat = {5000, 10000, 15000, 8000};
 
-        // Menampilkan daftar obat
-        System.out.println("Daftar Obat:");
-        for (int i = 0; i < daftarObat.length; i++) {
-            System.out.println((i + 1) + ". " + daftarObat[i] + " - Rp" + hargaObat[i]);
-        }
+        // Riwayat transaksi
+        String[] riwayatTransaksi = new String[20]; // Misalnya, menyimpan 10 transaksi terakhir
+        int transaksi = 0;
 
-        // Meminta pengguna memasukkan informasi pembelian
-        System.out.println("\nMasukkan informasi pembelian (nomor_obat jumlah_obat):");
-        System.out.print("Nomor obat yang dibeli: ");
-        int nomorObat = scanner.nextInt();
-        System.out.print("Jumlah obat yang dibeli: ");
-        int jumlahObat = scanner.nextInt();
+        // Variabel totalHarga perlu diinisialisasi di luar loop
+        double totalHarga = 0;
 
-        // Memeriksa apakah nomor obat valid
-        if (nomorObat < 1 || nomorObat > daftarObat.length) {
-            System.out.println("Nomor obat tidak valid.");
-            return;
-        }
+        do {
+            // Menampilkan daftar obat
+            System.out.println("Daftar Obat:");
+            for (int i = 0; i < daftarObat.length; i++) {
+                System.out.println((i + 1) + ". " + daftarObat[i] + " - Rp" + hargaObat[i]);
+            }
 
-        // Menghitung total harga obat yang dibeli
-        double totalHarga = hargaObat[nomorObat - 1] * jumlahObat;
+            // Meminta pengguna memasukkan informasi pembelian
+            System.out.println("\nMasukkan informasi pembelian (nomor_obat jumlah_obat):");
+            System.out.print("Nomor obat yang dibeli: ");
+            int nomorObat = scanner.nextInt();
 
-        // Menampilkan total harga obat yang dibeli
-        System.out.println("\nTotal harga obat yang dibeli: Rp" + totalHarga);
+            // Memeriksa apakah nomor obat valid
+            if (nomorObat < 1 || nomorObat > daftarObat.length) {
+                System.out.println("Nomor obat tidak valid.");
+                return;
+            }
+
+            System.out.print("Jumlah obat yang dibeli: ");
+            int jumlahObat = scanner.nextInt();
+
+            // Menambahkan informasi pembelian ke riwayatTransaksi
+            riwayatTransaksi[transaksi] = "Obat: " + daftarObat[nomorObat - 1] +
+                    ", Jumlah: " + jumlahObat +
+                    ", Total Harga: Rp" + (hargaObat[nomorObat - 1] * jumlahObat);
+            transaksi++;
+
+            // Menambahkan totalHarga berdasarkan informasi pembelian
+            totalHarga += hargaObat[nomorObat - 1] * jumlahObat;
+
+            // Menanyakan apakah pengguna ingin menambah item lagi
+            System.out.print("\nApakah Anda ingin menambah item lagi? (y/t): ");
+        } while (scanner.next().equalsIgnoreCase("y"));
 
         // Memeriksa apakah pengguna berhak mendapatkan diskon
+        double diskon = 0.0;
+
         if (totalHarga > 200000) {
             System.out.print("Apakah Anda memiliki member? (y/t): ");
             String member = scanner.next();
-            double diskon;
+
             if (member.equalsIgnoreCase("y")) {
                 diskon = 0.05;
-                totalHarga -= totalHarga * diskon;
                 System.out.println("Anda mendapatkan diskon sebesar 5%");
             } else {
                 diskon = 0.02;
-                totalHarga -= totalHarga * diskon;
                 System.out.println("Anda mendapatkan diskon sebesar 2%");
             }
+
+            totalHarga -= totalHarga * diskon;
         } else {
             System.out.println("Maaf, Anda tidak mendapatkan diskon.");
         }
@@ -56,12 +74,23 @@ public class kasir2 {
 
         // Menampilkan struk pembelian
         System.out.println("\nStruk Pembelian:");
-        System.out.println("Obat: " + daftarObat[nomorObat - 1]);
-        System.out.println("Harga per unit: Rp" + hargaObat[nomorObat - 1]);
-        System.out.println("Jumlah obat: " + jumlahObat);
-        System.out.println("Total Harga: Rp" + totalHarga);
+        for (int i = 0; i < transaksi; i++) {
+            System.out.println(riwayatTransaksi[i]);
+        }
+
+        // Menampilkan laporan transaksi atau riwayat penjualan
+        tampilkanLaporan(riwayatTransaksi, transaksi);
 
         // Menutup Scanner
         scanner.close();
+    }
+
+    // Fungsi untuk menampilkan laporan riwayat transaksi
+    private static void tampilkanLaporan(String[] riwayatTransaksi, int jumlahTransaksi) {
+        System.out.println("\nLaporan Transaksi:");
+
+        for (int i = 0; i < jumlahTransaksi; i++) {
+            System.out.println("Transaksi ke-" + (i + 1) + ": " + riwayatTransaksi[i]);
+        }
     }
 }
