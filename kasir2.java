cc63b995106107dcc608e2fb52a3f04ec5a577a9
farelvy <farelhajiri2@gmail.com>
2023-12-11 19:12:05 +0700

    - laporan & cek harga

--- kasir2.java
+++ kasir2.java
@@ -6,7 +6,7 @@ public class kasir2 {
 
         // Daftar obat dan harga
         String[] daftarObat = {"Paracetamol", "Vitamin C", "Antibiotik", "Obat Flu"};
-        double[] hargaObat = {5.0, 10.0, 15.0, 8.0};
+        int[] hargaObat = {5000, 10000, 15000, 8000};
 
         // Menampilkan daftar obat
         System.out.println("Daftar Obat:");
@@ -15,10 +15,10 @@ public class kasir2 {
         }
 
         // Meminta pengguna memasukkan informasi pembelian
-        System.out.println("\nMasukkan informasi pembelian (pisahkan dengan spasi):");
-        System.out.print("Nomor obat jumlah_obat: ");
+        System.out.println("\nMasukkan informasi pembelian (nomor_obat jumlah_obat):");
+        System.out.print("Nomor obat yang dibeli: ");
         int nomorObat = scanner.nextInt();
-        System.out.println("Masukkan jumlah obat: ");
+        System.out.print("Jumlah obat yang dibeli: ");
         int jumlahObat = scanner.nextInt();
 
         // Memeriksa apakah nomor obat valid
@@ -27,8 +27,32 @@ public class kasir2 {
             return;
         }
 
-        // Menghitung total harga
+        // Menghitung total harga obat yang dibeli
         double totalHarga = hargaObat[nomorObat - 1] * jumlahObat;
+
+        // Menampilkan total harga obat yang dibeli
+        System.out.println("\nTotal harga obat yang dibeli: Rp" + totalHarga);
+
+        // Memeriksa apakah pengguna berhak mendapatkan diskon
+        if (totalHarga > 200000) {
+            System.out.print("Apakah Anda memiliki member? (y/t): ");
+            String member = scanner.next();
+            double diskon;
+            if (member.equalsIgnoreCase("y")) {
+                diskon = 0.05;
+                totalHarga -= totalHarga * diskon;
+                System.out.println("Anda mendapatkan diskon sebesar 5%");
+            } else {
+                diskon = 0.02;
+                totalHarga -= totalHarga * diskon;
+                System.out.println("Anda mendapatkan diskon sebesar 2%");
+            }
+        } else {
+            System.out.println("Maaf, Anda tidak mendapatkan diskon.");
+        }
+
+        // Menampilkan total harga yang harus dibayar setelah diskon
+        System.out.println("Total yang harus Anda bayar: Rp" + totalHarga);
 
         // Menampilkan struk pembelian
         System.out.println("\nStruk Pembelian:");
@@ -36,7 +60,7 @@ public class kasir2 {
         System.out.println("Harga per unit: Rp" + hargaObat[nomorObat - 1]);
         System.out.println("Jumlah obat: " + jumlahObat);
         System.out.println("Total Harga: Rp" + totalHarga);
-        
+
         // Menutup Scanner
         scanner.close();
     }
